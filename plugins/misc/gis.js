41f35201ff5834cdadd2b651a2a44dd33b6645c4
Sourav <87891508+souravkl11@users.noreply.github.com>
2024-08-03 01:01:08 +0530

    fixed img, pinterest

--- plugins/misc/gis.js
+++ plugins/misc/gis.js
@@ -1,4 +1,4 @@
-const fetch = global.fetch || require("node-fetch");
+const fetch = global.fetch;
 const REGEX = /\["(\bhttps?:\/\/[^"]+)",(\d+),(\d+)\],null/g;
 
 /**
@@ -11,27 +11,48 @@ const REGEX = /\["(\bhttps?:\/\/[^"]+)",(\d+),(\d+)\],null/g;
  * @param {String} options.userAgent User agent for request
  * @returns {Promise<[{url: string, height: number, width: number }]>} Array of results
  */
-async function gis(searchTerm, options = {}) {
-    if (!searchTerm || typeof searchTerm !== "string") throw new TypeError("searchTerm must be a string.");
-    if (typeof options !== "object") throw new TypeError("options argument must be an object.");
+async function gis(searchTerm,limit, options = {}) {
+    if (!searchTerm || typeof searchTerm !== "string") return [];
+    if (typeof options !== "object") return [];
   
     const {
       query = {},
       userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
     } = options,
       body = await fetch(`http://www.google.com/search?${new URLSearchParams({ ...query, udm: "2", tbm: "isch", q: searchTerm })}`, { headers: { "User-Agent": userAgent } }).then(res => res.text()),
-      content = body.slice(body.lastIndexOf("ds:1"), body.lastIndexOf("sideChannel")),
-      results = [];
-  
+      content = body.slice(body.lastIndexOf("ds:1"), body.lastIndexOf("sideChannel"));  
     let result;
-  
-    while (result = REGEX.exec(content))
-      results.push({
-        url: result[1],
-        height: +result[2],
-        width: +result[3]
-      });
-  
-    return results;
-  }
-module.exports = {gis}
+    let urls = [];
+    let i = 0
+    while (result = REGEX.exec(content)){
+      if (i == limit) break;
+      urls.push(result[1]);
+      i++
+    }
+    return urls;
+  };
+  async function pinSearch(searchTerm,limit, options = {}) {
+    searchTerm = "pinterest " + searchTerm 
+      if (!searchTerm || typeof searchTerm !== "string") return [];
+      if (typeof options !== "object") return [];
+    
+      const {
+        query = {},
+        userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+      } = options,
+        body = await fetch(`http://www.google.com/search?${new URLSearchParams({ ...query, udm: "2", tbm: "isch", q: searchTerm })}`, { headers: { "User-Agent": userAgent } }).then(res => res.text()),
+        content = body.slice(body.lastIndexOf("ds:1"), body.lastIndexOf("sideChannel"));
+
+      let result;
+      let urls = [];
+      let i = 0
+      while (result = REGEX.exec(content)){
+        if (result[1].includes('pinimg.com')){
+        if (i == limit) break;
+        urls.push(result[1]);
+        i++
+        }
+      }
+      return urls;
+    };
+module.exports = {gis,pinSearch}
